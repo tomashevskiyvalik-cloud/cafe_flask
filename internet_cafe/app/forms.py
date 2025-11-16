09f9dca522e5683bcd5a5d75780d35c6b9fbe340
Валентин <tomashevskiyvalik@gmail.com>
2025-11-16 20:57:52 +0200

    add files

---
+++ internet_cafe/app/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, TextAreaField, SubmitField, EmailField, IntegerField
+from wtforms.validators import DataRequired, Email, NumberRange
+
+class TovarForm(FlaskForm):
+    nazva = StringField('Назва страви', validators=[DataRequired()])
+    opis = TextAreaField('Опис', validators=[DataRequired()])
+    tsina = FloatField('Ціна', validators=[DataRequired()])
+    submit = SubmitField('Зберегти')
+
+class FeedbackForm(FlaskForm):
+    name = StringField('Ім\'я', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Відгук', validators=[DataRequired()])
+    submit = SubmitField('Надіслати')
+
+class OrderForm(FlaskForm):
+    name = StringField('Ім\'я', validators=[DataRequired()])
+    phone = StringField('Телефон', validators=[DataRequired()])
+    quantity = IntegerField('Кількість', validators=[DataRequired(), NumberRange(min=1)], default=1)
+    submit = SubmitField('Замовити')
